849e21257e29cf890a064470a58cde1586c10b88
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-12-05 13:35:58 -0500

    Task 3 complete working on getting the links to work and open new tabs

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Menu data structure
+var menuLinks = [
+  {text: 'about', href: '/about.html'},
+  {text: 'catalog', href: '/catalog'},
+  {text: 'orders', href: '/orders'},
+  {text: 'account', href: '/account'},
+];
+
+
+
+
 let mainEl = document.querySelector('main');
 
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -8,6 +19,14 @@ let topMenuEl = document.querySelector('#top-menu');
 
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-topmMenuEl.className = 'flex-around'
+topMenuEl.className = 'flex-around'
 
 console.dir(topMenuEl)
+
+for(link of menuLinks) {
+  let a = document.createElement('a')
+  // a.href = link.href
+  // a.textContent = link.text
+  a.innerHTML = `<a href = '${link.href}'>${link.text}</a>`
+  topMenuEl.appendChild(a)
+}
